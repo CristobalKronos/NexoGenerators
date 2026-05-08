2a5db50aad6f7ef58d3a84b65a7602b24f3f33d5
CristóbalO_DonKronos <56177605+CristobalKronos@users.noreply.github.com>
2026-05-07 20:41:58 -0400

    Rename and logic
    Rename newPlanet and newStar, and base logic for newHex

--- generatores/newHex.py
+++ generatores/newHex.py
@@ -9,18 +9,37 @@ import random
     # Coordenadas Z
 
 # Un hex puede contener uno de los siguientes:
-    - Un sistema solar
-    - Nube de materia (nebulosa)
-    - Cementerio estelar
-    - Anomalía
-    - Vacío
+    - 40% Vacío
+    - 30% Un sistema solar
+    - 15% Nube de materia (nebulosa)
+    - 10% Cementerio estelar
+    - 5% Anomalía
 '''
 
 seed = random.seed()
 def verificar():
+    # Primero, verificar si el Hex está en la base de datos.
+    print("Verificando...")
+    print("Hex no registrado, procediendo a generar uno...")
+    generateHex(1,2,3)
+
+def generateHex(codX, codY, codZ):    
+    # Segundo, en caso de no haber un Hex creado, se genera uno nuevo.
+
     rng1 = random.randrange(100)
-    if rng1 >= 50:
-        print(rng1)
-    else:
+    print (rng1)
+    
+    if rng1 <= 40:
         print("Vacío")
+    elif rng1 <= 70:
+        print("Sistema solar")
+    elif rng1 <= 85:
+        print("Nube de materia / Nebulosa")
+    elif rng1 <= 95:
+        print("Cementerio estelar")
+    else:
+        print("Anomalía")
+    
+    # Tercero, se guarda el Hex en la base de datos.
+        
 verificar()
